9883d512dba628f6742e40abd7cdef9ee39c197c
irakli2206 <iraklibego1@gmail.com>
2022-09-05 11:07:30 +0400

    fully implemented formik

--- src/steps/FirstStep.js
+++ src/steps/FirstStep.js
@@ -1,91 +1,78 @@
 import React from 'react'
-import { Formik, Form, Field,ErrorMessage } from 'formik'
-import { useEffect,useState } from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { useEffect, useState, useRef } from 'react'
 import Axios from 'axios'
 
 const FirstStep = (props) => {
-    const [team, setTeam] = useState([])
-    const [posi, setPosi] = useState([])
-    const [teamid, setTeamid] = useState('')
-    const [listItems, setListItems] = useState(null)
-    
-    const handleSubmit = (values) => {
-        props.next(values)
-    }
-    useEffect(() => {
-    
-        Axios.get('https://pcfy.redberryinternship.ge/api/teams')
-           .then(res => {
-            console.log(res.data.data)
-             setTeam(res.data.data)
-             
-             
+  const [team, setTeam] = useState([])
+  const [posi, setPosi] = useState([])
 
-            
-            
-           })
-        
-         }, [])
-         
-         useEffect(() => {
-            Axios.get('https://pcfy.redberryinternship.ge/api/positions')
-           .then(res => {
-            console.log(res.data.data)
-             setPosi(res.data.data)
-             
+  const handleSubmit = (values) => {
+    props.next(values)
+  }
+  useEffect(() => {
 
-            
-            
-           })
-         },[])
-         useEffect(() => {
-            const list = () => posi.filter(p => p.team_id == teamid)
-            .map((p) => <option key={p.id}  label={p.name} >{p.name}</option> )
-            setListItems(list)
-         },[teamid])
-         console.log('teamid - ' , teamid)
-         
+    Axios.get('https://pcfy.redberryinternship.ge/api/teams')
+      .then(res => {
+        console.log(res.data.data)
+        setTeam(res.data.data)
+      })
+
+  }, [])
+
+  useEffect(() => {
+    Axios.get('https://pcfy.redberryinternship.ge/api/positions')
+      .then(res => {
+        console.log(res.data.data)
+        setPosi(res.data.data)
+
+      })
+  }, [])
+
+
   return (
     <Formik
-    initialValues={props.data}
-    onSubmit ={handleSubmit}
+      initialValues={props.data}
+      onSubmit={handleSubmit}
     >
-        {() => (
-            <Form>
-                <p>saxeli</p>
-                <Field name='first_name'/>
-                <p>gvari</p>
-                <Field name='last_name'/>
-                <p>airchie</p>
-                <select  name ='team' onChange = {(e) => setTeamid(e.target.value)} >
-                  <option value="select">--select--</option>
-                {team.map((pos) => <option key={pos.id} value ={pos.id} >{pos.name}</option> )}
-                    
-                    
-                </select><br />
-                <select  name="position">
-                {listItems}
-                    
-                    
-                </select>
-                
+      {({values}) => (
+        <Form>
+          <p>saxeli</p>
+          <Field name='first_name' />
+          <p>gvari</p>
+          <Field name='last_name' />
+          <p>airchie</p>
+          <Field as="select" name='team'  >
+            <option value="select" >--select--</option>
+            {team.map((pos) => <option key={pos.id} value={pos.id} >{pos.name}</option>)}
 
 
-                <p>maili</p>
-                <Field 
-                id="email"
-                name="email"
-                placeholder="jane@acme.com"
-                type="email"
-                />
-                
-                <p>telefonis nomeri</p>
-                <Field name ='number'/>
-                <br/>
-                <button type='submit' >shemdegi</button>
+          </Field><br />
+          {values.team && 
+          <Field as="select" name="position">
+            <option value="select" >--select--</option>
+            {posi.filter(p => p.team_id == values.team)
+              .map((p) => <option key={p.id} value={p.name} label={p.name} >{p.name}</option>)}
+          </Field>
+          }
 
-            </Form>
-        )}
+
+
+          <p>maili</p>
+          <Field
+            id="email"
+            name="email"
+            placeholder="jane@acme.com"
+            type="email"
+          />
+
+          <p>telefonis nomeri</p>
+          <Field name='number' />
+          <br />
+          <button type='submit' >shemdegi</button>
+
+        </Form>
+      )}
     </Formik>
   )
 }
